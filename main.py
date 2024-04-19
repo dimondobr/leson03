1aee5667e95f0121baa369e9413d5f8adf0240e5
dimondobr <166716116+dimondobr@users.noreply.github.com>
2024-04-19 23:32:24 +1000

    ne ponima

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ pygame.display.set_icon(icon)
 
 target_ing = pygame.image.load("img/target.png")
 target_width = 72
-terget_height = 80
+target_height = 80
 
-terget_x = random.randint(0,SCREEN_WIDTH - target_width)
-target_y = random.randint( 0, SCREEN_HEISHT - terget_height)
+target_x = random.randint(0,SCREEN_WIDTH - target_width)
+target_y = random.randint( 0, SCREEN_HEISHT - target_height)
 
 color = "light green"
 
@@ -28,10 +28,10 @@ while running:
             runnning = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            if terget_x < mouse_x < terget_x - target_width and target_y < mouse_y < target_y - terget_height:
-                terget_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEISHT - terget_height)
-    screen.blit(target_ing, (terget_x, target_y))
+            if target_x < mouse_x < target_x - target_width and target_y < mouse_y < target_y - target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEISHT - target_height)
+    screen.blit(target_ing, (target_x, target_y))
     pygame.display.update()
 
 pygame.quit()
