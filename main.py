b8b30096cc9e72a3331c6ee5357554f383060015
dimondobr <166716116+dimondobr@users.noreply.github.com>
2024-04-20 00:27:02 +1000

    &&&&

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
+import time
 import random
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREEN_HEISHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEISHT))
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("игра Тир")
 icon = pygame.image.load("img/klipartz.com.png")
@@ -15,7 +16,7 @@ target_width = 72
 target_height = 80
 
 target_x = random.randint(0,SCREEN_WIDTH - target_width)
-target_y = random.randint( 0, SCREEN_HEISHT - target_height)
+target_y = random.randint( 0, SCREEN_HEIGHT - target_height)
 
 color = "light green"
 
@@ -25,12 +26,12 @@ while running:
     screen.fill(color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            runnning = False
+            running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            if target_x < mouse_x < target_x - target_width and target_y < mouse_y < target_y - target_height:
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEISHT - target_height)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     screen.blit(target_ing, (target_x, target_y))
     pygame.display.update()
 
