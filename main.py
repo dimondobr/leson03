f0f924355d8a51c67ba254f5ff601fc72432a863
dimondobr <166716116+dimondobr@users.noreply.github.com>
2024-04-21 09:23:37 +1000

    muki tvorchestva

--- main.py
+++ main.py
@@ -21,14 +21,25 @@ target_y = random.randint( 0, SCREEN_HEIGHT - target_height)
 
 color = "light green"
 font = pygame.font.Font(None, 36)
+clock = pygame.time.Clock()
+
+wait_time = 500  # Время ожидания в миллисекундах (0.5 секунд)
+start_time = pygame.time.get_ticks()
+
 
 running = True
 while running:
     screen.fill(color)
+    current_time = pygame.time.get_ticks()
+    elapsed_time = current_time - start_time
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN :
+        if elapsed_time >= wait_time:
+            target_x = random.randint(0, SCREEN_WIDTH - target_width)
+            target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+        elif event.type == pygame.MOUSEBUTTONDOWN :
             mouse_x, mouse_y = pygame.mouse.get_pos()
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
@@ -38,12 +49,12 @@ while running:
                time.sleep(0.5)
                target_x = random.randint(0, SCREEN_WIDTH - target_width)
                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+        elif  elapsed_time >= wait_time:
+            target_x = random.randint(0, SCREEN_WIDTH - target_width)
+            target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     text_x = font.render(f'попал: {x}', True, (0, 0, 0))
     screen.blit(text_x, (50, 50))
 
-
-
-    pygame.display.flip()
 
     screen.blit(target_ing, (target_x, target_y))
     pygame.display.update()
