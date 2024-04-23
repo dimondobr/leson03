d18e194352bbfb81f161e70942b66647f6362144
dimondobr <166716116+dimondobr@users.noreply.github.com>
2024-04-24 01:19:14 +1000

    kak bulo

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-n"
+import pygame
+import time
+import random
+pygame.init()
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("игра Тир")
+icon = pygame.image.load("img/klipartz.com.png")
+pygame.display.set_icon(icon)
+
+target_ing = pygame.image.load("img/target.png")
+target_width = 72
+target_height = 80
+x = 0
+target_x = random.randint(0,SCREEN_WIDTH - target_width)
+target_y = random.randint( 0, SCREEN_HEIGHT - target_height)
+
+color = "light green"
 font = pygame.font.Font(None, 36)
 clock = pygame.time.Clock()
 
