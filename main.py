33a7439d88fb6fd4ee2acb01b40e1c27f341eb64
dimondobr <166716116+dimondobr@users.noreply.github.com>
2024-04-24 01:07:31 +1000

    002

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-pygame.display.set_caption("hghjfljflhf")
 icon = pygame.image.load("img/klipartz.com.png")
 pygame.display.set_icon(icon)
 
 target_ing = pygame.image.load("img/target.png")
 target_width = 72
 target_height = 80
-x = 0
+x = 900
 target_x = random.randint(0,SCREEN_WIDTH - target_width)
 target_y = random.randint( 0, SCREEN_HEIGHT - target_height)
 
