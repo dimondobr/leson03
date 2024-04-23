b906a51e19f52bb11ee70a68ac3e775c55263143
dimondobr <166716116+dimondobr@users.noreply.github.com>
2024-04-24 01:05:54 +1000

    000

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import time
 import random
 pygame.init()
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("игра Тир")
