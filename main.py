8730828a493e4338179da4a737a59edee3b21285
dimondobr <166716116+dimondobr@users.noreply.github.com>
2024-04-24 01:12:00 +1000

    006

--- main.py
+++ main.py
@@ -3,21 +3,7 @@ import time
 import random
 pygame.init()
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-icon = pygame.image.load("img/klipartz.com.png")
-pygame.display.set_icon(icon)
-
-target_ing = pygame.image.load("img/target.png")
-target_width = 72
-target_height = 80
-x = 900.for to 800
-target_x = random.randint(0,SCREEN_WIDTH - target_width)
-target_y = random.randint( 0, SCREEN_HEIGHT - target_height)
-
-color = "light green"
+n"
 font = pygame.font.Font(None, 36)
 clock = pygame.time.Clock()
 
